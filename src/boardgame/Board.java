795ec8af3078acf3605eeab4f6ad312f3110ce13
renatomeireles <renato.martins.meireles@gmail.com>
2024-03-12 20:00:26 -0300

    Adding rook and king, board exception, and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int colunms) {
+		if (rows < 1 || colunms < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and column");
+		}
 		this.rows = rows;
 		this.colunms = colunms;
 		pieces = new Piece[rows][colunms];
@@ -15,21 +18,45 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
 	public int getColunms() {
 		return colunms;
 	}
-	public void setColunms(int colunms) {
-		this.colunms = colunms;
-	}
 	
 	public Piece piece(int row, int colunm) {
+		if(!positionExists(row, colunm)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][colunm];
 	}
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColunm()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on this position" + position);
+		}
+		pieces[position.getRow()][position.getColunm()] = piece;
+		piece.position = position;
+		
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < colunms;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColunm());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
 	}
 	
 }
